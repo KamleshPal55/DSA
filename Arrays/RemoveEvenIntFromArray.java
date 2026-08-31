bf82e44acd942a942ab066047f7f139e81868b1b
Kamlesh Pal <KamleshPal55@yahoo.com>
2026-08-30 19:47:28 -0700

    Added RemoveEvenIntFromArray class

---
+++ Arrays/RemoveEvenIntFromArray.java
@@ -0,0 +1,33 @@
+/*
+ * I/P =>{3,2,4,7,10,6,5}  O/P => {3,7,5}
+ */
+
+package Arrays;
+
+import java.util.Arrays;
+
+public class RemoveEvenIntFromArray {
+	public static void main(String[] args) {
+	int[] arr = {3,2,4,7,10,6,5};
+	int[] oddNumArr = removeEvenInt(arr);
+	System.out.println(Arrays.toString(oddNumArr));
+	}
+
+	public static int[] removeEvenInt(int[] arr) {
+		int oddCount = 0;
+		for(int num : arr) {
+			if(num % 2 != 0) {
+				oddCount++;
+			}
+		}
+		int[] result = new int[oddCount];
+		int idx = 0;
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i] % 2 != 0) {
+				result[idx] = arr[i];
+				idx++;
+			}
+		}
+		return result;
+	}
+}
